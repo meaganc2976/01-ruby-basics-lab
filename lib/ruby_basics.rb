433a742a7be57ae6b9a0b4829be4580670cc6624
meaganc2976 <meaganc2976@hstat.org>
2016-10-12 18:17:15 +0000

    hw

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,24 @@
-def division
-    
+def division(num1, num2)
+    num1.to_i / num2.to_i
 end
 
-def argue
+def argue(a)
+    return a
   
 end
 
-def greeting
-
+def greeting(greeting, name)
+"#{greeting} #{name}!"
 end
 
 def return_a_value
-    
+    "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(food = "cheese")
+    food
 end
 
-def whisper
-    
+def whisper(word)
+    word.downcase
 end
